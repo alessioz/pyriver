06916ce4929f2d55b449d2145554c4e2f675c65a
Alessio Zanol <nardei@infinito.it>
2010-03-13 02:02:39 +0100

    scritto init della classe Section

--- geometry.py
+++ geometry.py
@@ -1,1 +1,11 @@
- 
+# -*- coding: utf-8 -*-
+class Section:
+    def __init_(self, xaxis=None,
+		yzcoordinates=None, erodible=True,
+		roughness=None, discontinuity=False):
+	self.xaxis = xaxis
+	self.yzcoordinates= yzcoordinates
+	self.erodible = erodible
+	self.roughness = roughness
+	self.discontinuity = discontinuity
+	self.segments = []
