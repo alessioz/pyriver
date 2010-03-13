e6872ac52fbc076613edffae2c9e430a7873de35
Alessio Zanol <nardei@infinito.it>
2010-03-13 14:18:39 +0100

    Created Section class and Segment of a section class. It's possible to create a list of segments inside the section. Each segment with its own roughness. Problems: 1. different segments can share the same part of the section. 2. is it possible to rewrite all in a more simple and elegant way?

--- geometry.py
+++ geometry.py
@@ -1,11 +1,39 @@
 # -*- coding: utf-8 -*-
+
+class Segment:
+    """
+    It defines the segment of a river cross-section
+    with its own roughness.
+    """
+    def __init__(self, 
+		yzcoordSegm=None,
+		roughness=None):
+	self.yzcoordSegm = yzcoordSegm
+	self.roughness = roughness
+
 class Section:
-    def __init_(self, xaxis=None,
-		yzcoordinates=None, erodible=True,
+    """
+    It defines attributes and methods for a river cross-section.
+    It's possible to define sub-segments of the section,
+    each one with a different roughness.
+    Example of usage:
+	
+	coord = [[0,10],[0,0],[10,0],[20,0],[20,10]]
+	sect = Section(0, coord)
+	sect.addSegment(sect.yzcoord[0:2], 35)
+	sect.addSegment(sect.yzcoord[2:], 40)
+    """
+    def __init__(self, xaxis=None,
+		yzcoord=None, erodible=True,
 		roughness=None, discontinuity=False):
 	self.xaxis = xaxis
-	self.yzcoordinates= yzcoordinates
+	self.yzcoord = yzcoord
 	self.erodible = erodible
 	self.roughness = roughness
 	self.discontinuity = discontinuity
-	self.segments = []
+	self.segment = []
+
+    def addSegment(self, yzcoordSegm=None,
+		    roughness=None):
+	segment = Segment(yzcoordSegm, roughness)
+	self.segment.append(segment)
